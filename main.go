13d30b6c6151fec6b73595fa944ee37fedb7ae28
Matt Folsom <msf930@gmail.com>
2026-01-07 12:46:31 -0700

    gpt paste main

--- main.go
+++ main.go
@@ -1,88 +1,148 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+var articleType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Article",
+		Fields: graphql.Fields{
+			"title":   &graphql.Field{Type: graphql.String},
+			"desc":    &graphql.Field{Type: graphql.String},
+			"content": &graphql.Field{Type: graphql.String},
+		},
+	},
+)
+
+var articles = []map[string]string{
+	{
+		"title":   "Hello World",
+		"desc":    "First article",
+		"content": "This is content",
+	},
 }
 
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		{Title: "Test Title", Desc: "Test Description", Content: "Test Content"},
-	}
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the HomePage!")
-}
-
-// CORS middleware
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// Presign handler
-func presignHandler(w http.ResponseWriter, r *http.Request) {
-	filename := r.URL.Query().Get("filename")
-	contentType := r.URL.Query().Get("contentType")
-
-	if filename == "" || contentType == "" {
-		http.Error(w, "missing filename or contentType", http.StatusBadRequest)
-		return
-	}
-
-	url, err := getPresignedURL(filename, contentType)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"url": url,
-	})
-}
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"articles": &graphql.Field{
+			Type: graphql.NewList(articleType),
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return articles, nil
+			},
+		},
+	},
+})
 
 func main() {
-	mux := http.NewServeMux()
-
-	// REST endpoints
-	mux.HandleFunc("/", homePage)
-	mux.HandleFunc("/articles", allArticles)
-	mux.HandleFunc("/presign", presignHandler)
-
-	// GraphQL handler
-	h := handler.New(&handler.Config{
-		Schema:   &schema, // your graphql schema
-		Pretty:   true,
-		GraphiQL: true,
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
 	})
-	mux.Handle("/graphql", h)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Println("Server running on http://localhost:8082")
-	// log.Fatal(http.ListenAndServe(":8082", enableCORS(mux)))
+	h := handler.New(&handler.Config{
+		Schema: &schema,
+		Pretty: true,
+	})
+
+	http.Handle("/graphql", h)
+
+	log.Println("Server running on :8082")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8082", nil))
 }
+
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/graphql-go/handler"
+// )
+
+// type Article struct {
+// 	Title   string `json:"Title"`
+// 	Desc    string `json:"desc"`
+// 	Content string `json:"content"`
+// }
+
+// type Articles []Article
+
+// func allArticles(w http.ResponseWriter, r *http.Request) {
+// 	articles := Articles{
+// 		{Title: "Test Title", Desc: "Test Description", Content: "Test Content"},
+// 	}
+// 	json.NewEncoder(w).Encode(articles)
+// }
+
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Welcome to the HomePage!")
+// }
+
+// // CORS middleware
+// func enableCORS(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
+// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+// 		if r.Method == http.MethodOptions {
+// 			w.WriteHeader(http.StatusOK)
+// 			return
+// 		}
+
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
+
+// // Presign handler
+// func presignHandler(w http.ResponseWriter, r *http.Request) {
+// 	filename := r.URL.Query().Get("filename")
+// 	contentType := r.URL.Query().Get("contentType")
+
+// 	if filename == "" || contentType == "" {
+// 		http.Error(w, "missing filename or contentType", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	url, err := getPresignedURL(filename, contentType)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(map[string]string{
+// 		"url": url,
+// 	})
+// }
+
+// func main() {
+// 	mux := http.NewServeMux()
+
+// 	// REST endpoints
+// 	mux.HandleFunc("/", homePage)
+// 	mux.HandleFunc("/articles", allArticles)
+// 	mux.HandleFunc("/presign", presignHandler)
+
+// 	// GraphQL handler
+// 	h := handler.New(&handler.Config{
+// 		Schema:   &schema, // your graphql schema
+// 		Pretty:   true,
+// 		GraphiQL: true,
+// 	})
+// 	mux.Handle("/graphql", h)
+
+// 	log.Println("Server running on http://localhost:8082")
+// 	// log.Fatal(http.ListenAndServe(":8082", enableCORS(mux)))
+// 	log.Fatal(http.ListenAndServe("0.0.0.0:8082", nil))
+// }
--- schema.go
+++ schema.go
@@ -1,77 +1,77 @@
 package main
 
-import "github.com/graphql-go/graphql"
+// import "github.com/graphql-go/graphql"
 
-var articles = Articles{
-	{
-		Title:   "Test Title",
-		Desc:    "Test Description",
-		Content: "Test Content",
-	},
-}
+// var articles = Articles{
+// 	{
+// 		Title:   "Test Title",
+// 		Desc:    "Test Description",
+// 		Content: "Test Content",
+// 	},
+// }
 
-var articleType = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "Article",
-		Fields: graphql.Fields{
-			"title":   &graphql.Field{Type: graphql.String},
-			"desc":    &graphql.Field{Type: graphql.String},
-			"content": &graphql.Field{Type: graphql.String},
-		},
-	},
-)
+// var articleType = graphql.NewObject(
+// 	graphql.ObjectConfig{
+// 		Name: "Article",
+// 		Fields: graphql.Fields{
+// 			"title":   &graphql.Field{Type: graphql.String},
+// 			"desc":    &graphql.Field{Type: graphql.String},
+// 			"content": &graphql.Field{Type: graphql.String},
+// 		},
+// 	},
+// )
 
-// ---------- QUERY ----------
-var rootQuery = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			"articles": &graphql.Field{
-				Type: graphql.NewList(articleType),
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return articles, nil
-				},
-			},
-		},
-	},
-)
+// // ---------- QUERY ----------
+// var rootQuery = graphql.NewObject(
+// 	graphql.ObjectConfig{
+// 		Name: "Query",
+// 		Fields: graphql.Fields{
+// 			"articles": &graphql.Field{
+// 				Type: graphql.NewList(articleType),
+// 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+// 					return articles, nil
+// 				},
+// 			},
+// 		},
+// 	},
+// )
 
-// ---------- MUTATION ----------
-var rootMutation = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "Mutation",
-		Fields: graphql.Fields{
-			"createArticle": &graphql.Field{
-				Type: articleType,
-				Args: graphql.FieldConfigArgument{
-					"title": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"desc": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-					"content": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-				},
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					article := Article{
-						Title:   p.Args["title"].(string),
-						Desc:    p.Args["desc"].(string),
-						Content: p.Args["content"].(string),
-					}
+// // ---------- MUTATION ----------
+// var rootMutation = graphql.NewObject(
+// 	graphql.ObjectConfig{
+// 		Name: "Mutation",
+// 		Fields: graphql.Fields{
+// 			"createArticle": &graphql.Field{
+// 				Type: articleType,
+// 				Args: graphql.FieldConfigArgument{
+// 					"title": &graphql.ArgumentConfig{
+// 						Type: graphql.NewNonNull(graphql.String),
+// 					},
+// 					"desc": &graphql.ArgumentConfig{
+// 						Type: graphql.String,
+// 					},
+// 					"content": &graphql.ArgumentConfig{
+// 						Type: graphql.String,
+// 					},
+// 				},
+// 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+// 					article := Article{
+// 						Title:   p.Args["title"].(string),
+// 						Desc:    p.Args["desc"].(string),
+// 						Content: p.Args["content"].(string),
+// 					}
 
-					articles = append(articles, article)
-					return article, nil
-				},
-			},
-		},
-	},
-)
+// 					articles = append(articles, article)
+// 					return article, nil
+// 				},
+// 			},
+// 		},
+// 	},
+// )
 
-var schema, _ = graphql.NewSchema(
-	graphql.SchemaConfig{
-		Query:    rootQuery,
-		Mutation: rootMutation,
-	},
-)
+// var schema, _ = graphql.NewSchema(
+// 	graphql.SchemaConfig{
+// 		Query:    rootQuery,
+// 		Mutation: rootMutation,
+// 	},
+// )
